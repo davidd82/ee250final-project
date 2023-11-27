04e8085219d0ee77037f4e09427092562f8779a0
David Delgado <davidd82@usc.edu>
2023-11-27 12:56:33 -0800

    update

--- server/server.py
+++ server/server.py
@@ -1,5 +1,10 @@
+import paho.mqtt.client as mqtt
+import time
 from flask import Flask, render_template
 app = Flask(__name__)
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected to server (i.e., broker) with result code "+str(rc))
 
 @app.route('/')
 def index():
@@ -7,9 +12,18 @@ def index():
 
 @app.route('/my-link/')
 def my_link():
-  print ('I got clicked!')
+  print ('Getting status')
 
+  client = mqtt.Client()
+  client.connect(host= "test.mosquitto.org", port= 1883, keepalive=60)
+  print ('Connected to server')
+  client.loop_start()
+  client.publish("davidd82/light", "LIGHT_STATUS")
+  print('Published message')
+
+  
   return 'Click.'
 
 if __name__ == '__main__':
   app.run(debug=True)
+  
