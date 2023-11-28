c83c27eb4390457ebe0a9c7ec9d710e8874ecf17
Xiangbo Qi <michaelqi@Xiangbos-MacBook-Air.local>
2023-11-27 17:15:51 -0800

    b

--- server/server.py
+++ server/server.py
@@ -3,6 +3,9 @@ import time
 from flask import Flask, render_template
 app = Flask(__name__)
 
+def avaibility(client, userdata, message):
+  return message
+
 def on_connect(client, userdata, flags, rc):
   print("Connected to server (i.e., broker) with result code "+str(rc))
 
@@ -17,6 +20,8 @@ def my_link():
   client = mqtt.Client()
   client.connect(host= "test.mosquitto.org", port= 1883, keepalive=60)
   print ('Connected to server')
+  client.subscribe("davidd82/available")
+  message = client.message_callback_add("davidd82/available", avaibility)
   client.loop_start()
   client.publish("davidd82/light_and_sound", "STATUS")
   client.publish("davidd82/csv", "MAKE_CSV")
@@ -24,7 +29,7 @@ def my_link():
   print('Published message')
 
   
-  return 'Click.'
+  return str(message)
 
 if __name__ == '__main__':
   app.run(debug=True)
